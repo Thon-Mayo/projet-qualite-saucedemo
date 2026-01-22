519a8bef65c4ce329a02fc178330ff2b1df0fa46
Evan <164463587+Thon-Mayo@users.noreply.github.com>
2026-01-22 19:46:49 +0100

    Delete projet-qualite-playwright-saucedemo directory

--- projet-qualite-playwright-saucedemo/tests/pages/CheckoutCompletePage.ts
+++
@@ -1,9 +0,0 @@
-import { Page, expect } from '@playwright/test';
-
-export class CheckoutCompletePage {
-  constructor(private page: Page) {}
-
-  async expectThankYou() {
-    await expect(this.page.locator('.complete-header')).toHaveText('Thank you for your order!');
-  }
-}
